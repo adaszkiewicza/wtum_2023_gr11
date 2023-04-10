de6ddd925cfd20654af5a0cfb99e2f12d932ced0
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-04-10 02:16:14 +0200

    Fixed image refreshing after choosing another one

--- main_temp.py
+++ main_temp.py
@@ -2,15 +2,20 @@ from tkinter import *
 from PIL import Image, ImageTk
 import easygui
 
+
 def load_image():
     global image 
     global left_frame
     global root
+    global label1
+    global label2
     
     filename = easygui.fileopenbox(filetypes=["*.gif"])
     imaget = Image.open(filename)
     imaget = imaget.resize((400,400))
     image = ImageTk.PhotoImage(imaget)
+    label1 = Label(left_frame, image=image).grid(row=1, column=0, padx=5, pady=5)
+    label2 = Label(right_frame, image=image).grid(row=1,column=0, padx=5, pady=5)
 
 root = Tk()  # create root window
 root.title("Basic GUI Layout")  # title of the GUI window
@@ -37,10 +42,9 @@ imaget = Image.open("images/wow.gif")
 imaget = imaget.resize((400,400))
 
 image = ImageTk.PhotoImage(imaget)
-Label(left_frame, image=image).grid(row=1, column=0, padx=5, pady=5)
+label1 = Label(left_frame, image=image).grid(row=1, column=0, padx=5, pady=5)
+label2 = Label(right_frame, image=image).grid(row=1,column=0, padx=5, pady=5)
 
-# Display image in right_frame
-Label(right_frame, image=image).grid(row=1,column=0, padx=5, pady=5)
 Button(bottom_frame, text="LOAD IMAGE", command=load_image).grid(row=0, column=0, padx=5, pady=5)
 Checkbutton(bottom_frame, text='Is generated from noise', onvalue=1, offvalue=0).grid(row=0,column=1,padx=5,pady=5)
 Button(bottom_frame, text="EXIT APPLICATION", command=root.quit).grid(row=0, column=2, padx=5, pady=5)
