9fd1a67f3b75da1d8947a6bfabda9d4a7b81249f
Antoni Adaszkiewicz <ada.antek@gmail.com>
2023-05-06 19:42:34 +0200

    Refactor gui code

--- main.py
+++ main.py
@@ -1,116 +1,136 @@
-from tkinter import *
-from PIL import Image, ImageTk
 import easygui
-from gan import gan_function
 import numpy as np
 import tensorflow as tf
+from tkinter import *
+from PIL import Image, ImageTk
+from gan import gan_function
+from enum import Enum
 
-MONET = 0
-VANGOGH = 1
-UKIYOE = 2
 EXAMPLE_PATH = "images/example.jpg"
 IMAGE_DISPLAY_SIZE = (400, 400)
 
-def preprocess_image(img):
-    img = tf.io.decode_image(img)
-    img_rows, img_cols, channel = 256, 256, 3
-    return tf.reshape(tf.cast(tf.image.resize(img, (int(img_rows), int(img_cols))), tf.float32) / 127.5 - 1,
-                      (1, img_rows, img_cols, channel))
+class Painters(Enum):
+    MONET = 0
+    VANGOGH = 1
+    UKIYOE = 2
 
-def generate_picture(img):
-    selected_index = selected_artist.curselection()
-    if selected_index:
-        index = selected_index[0]
-        if index == 0:
-            prediction = monet_generator(img, training=False)[0].numpy()
+class App:
+    def __init__(self):
+        self.setup_generators()
+        self.setup_root()
+        self.setup_listbox()
+        self.setup_image_first()
 
-        elif index == 1:
-            prediction = vangogh_generator(img, training=False)[0].numpy()
+    def run(self):
+        self.root.mainloop()
 
-        elif index == 2:
-            prediction = ukiyoe_generator(img, training=False)[0].numpy()
+    def setup_generators(self):
+        self.monet_generator = gan_function(Painters.MONET.value)
+        self.vangogh_generator = gan_function(Painters.VANGOGH.value)
+        self.ukiyoe_generator = gan_function(Painters.UKIYOE.value)
 
-    else:
-        prediction = monet_generator(img, training=False)[0].numpy()
+    def setup_root(self):
+        self.root = Tk()
+        self.root.title("Basic GUI Layout")  # title of the GUI window
+        self.root.maxsize(900, 600)  # specify the max size the window can expand to
+        self.root.config(bg="skyblue")  # specify background color
 
-    prediction = (prediction * 127.5 + 127.5).astype(np.uint8)
-    generated_img = Image.fromarray(prediction)
-    generated_img = generated_img.resize(IMAGE_DISPLAY_SIZE)
-    generated_img = ImageTk.PhotoImage(generated_img)
+        # Create left and right frames
+        self.left_frame = Frame(self.root, width=200, height=400, bg='grey')
+        self.left_frame.grid(row=0, column=0, padx=10, pady=5)
 
-    label2 = Label(right_frame, image=generated_img)
-    label2.image = generated_img
-    label2.grid(row=1, column=0, padx=5, pady=5)
+        self.right_frame = Frame(self.root, width=650, height=400, bg='grey')
+        self.right_frame.grid(row=0, column=1, padx=10, pady=5)
 
-def selected_artist_changed():
-    generate_picture(original_img)
+        self.bottom_frame = Frame(self.root, width = 880, height=100, bg='grey')
+        self.bottom_frame.grid(row=1,columnspan=2, padx=10, pady=5)
 
-def load_image():
-    global image
-    global left_frame
-    global root
-    global label1
-    global label2
-    global original_img
+        # Create frames and labels in left_frame
+        Label(self.left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
+        # Create frames and labels in right_frame
+        Label(self.right_frame, text="Result Image").grid(row=0, column=0, padx=5, pady=5)
 
-    filename = easygui.fileopenbox(filetypes=["*.png", "*.jpg"]) # need to change so that they are both in one tab and
-                        # so that only files of these types can be selected (still better than GIFs, so its improvement)
-    image = Image.open(filename)
-    image = image.resize(IMAGE_DISPLAY_SIZE)
-    image = ImageTk.PhotoImage(image)
-    label1 = Label(left_frame, image=image).grid(row=1, column=0, padx=5, pady=5)
+        Button(self.bottom_frame, text="LOAD IMAGE", command=self.load_image).grid(row=0, column=0, padx=5, pady=5)
 
-    img_to_generate_painting = tf.io.read_file(filename)
-    img_to_generate_painting = preprocess_image(img_to_generate_painting)
-    original_img = img_to_generate_painting
-    generate_picture(img_to_generate_painting)
+        Checkbutton(self.bottom_frame, text='Is generated from noise', onvalue=1, offvalue=0).grid(row=0, column=1, padx=5, pady=5)
 
-def first_image_generate(PATH):
-    global original_img
-    img_to_generate_painting = tf.io.read_file(PATH)
-    img_to_generate_painting = preprocess_image(img_to_generate_painting)
-    original_img = img_to_generate_painting
-    generate_picture(img_to_generate_painting)
+    def setup_listbox(self):
+        self.selected_artist = Listbox(self.bottom_frame, height=len(Painters))
+        self.selected_artist.grid(row=0, column=2, padx=5, pady=5)
+        self.selected_artist.insert(Painters.MONET.value, "Monet")
+        self.selected_artist.insert(Painters.VANGOGH.value, "Van Gogh")
+        self.selected_artist.insert(Painters.UKIYOE.value, "Ukiyoe")
+        self.selected_artist.selection_set(0)
+        self.selected_artist.bind("<<ListboxSelect>>", lambda event: self.selected_artist_changed())
 
-root = Tk()  # create root window
-root.title("Basic GUI Layout")  # title of the GUI window
-root.maxsize(900, 600)  # specify the max size the window can expand to
-root.config(bg="skyblue")  # specify background color
+    def setup_image_first(self):
+        image = Image.open(EXAMPLE_PATH)
+        image = image.resize(IMAGE_DISPLAY_SIZE)
+        image = ImageTk.PhotoImage(image)
 
-# Create left and right frames
-left_frame = Frame(root, width=200, height=400, bg='grey')
-left_frame.grid(row=0, column=0, padx=10, pady=5)
+        self.label_original = Label(self.left_frame, image=image)
+        self.label_original.image = image
+        self.label_original = self.label_original.grid(row=1, column=0, padx=5, pady=5)
 
-right_frame = Frame(root, width=650, height=400, bg='grey')
-right_frame.grid(row=0, column=1, padx=10, pady=5)
+        self.first_image_generate(EXAMPLE_PATH)
 
-bottom_frame = Frame(root, width = 880, height=100, bg='grey')
-bottom_frame.grid(row=1,columnspan=2, padx=10, pady=5)
+    def first_image_generate(self, PATH):
+        img_to_generate_painting = tf.io.read_file(PATH)
+        img_to_generate_painting = self.preprocess_image(img_to_generate_painting)
+        self.original_img = img_to_generate_painting
+        self.generate_picture(img_to_generate_painting)
 
-# Create frames and labels in left_frame
-Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
-# Create frames and labels in right_frame
-Label(right_frame, text="Result Image").grid(row=0, column=0, padx=5, pady=5)
+    def generate_picture(self, img):
+        selected_index = self.selected_artist.curselection()
+        if selected_index:
+            index = selected_index[0]
+            if index == 0:
+                prediction = self.monet_generator(img, training=False)[0].numpy()
 
-Button(bottom_frame, text="LOAD IMAGE", command=load_image).grid(row=0, column=0, padx=5, pady=5)
-Checkbutton(bottom_frame, text='Is generated from noise', onvalue=1, offvalue=0).grid(row=0, column=1, padx=5, pady=5)
-selected_artist = Listbox(bottom_frame, height=3)
-selected_artist.grid(row=0, column=2, padx=5, pady=5)
-selected_artist.insert(0, "Monet")
-selected_artist.insert(1, "Van Gogh")
-selected_artist.insert(2, "Ukiyoe")
-selected_artist.selection_set(0)
-selected_artist.bind("<<ListboxSelect>>", lambda event: selected_artist_changed())
+            elif index == 1:
+                prediction = self.vangogh_generator(img, training=False)[0].numpy()
 
-monet_generator = gan_function(MONET)
-vangogh_generator = gan_function(VANGOGH)
-ukiyoe_generator = gan_function(UKIYOE)
+            elif index == 2:
+                prediction = self.ukiyoe_generator(img, training=False)[0].numpy()
 
-# load image to be "edited"
-image = Image.open(EXAMPLE_PATH)
-image = image.resize(IMAGE_DISPLAY_SIZE)
-image = ImageTk.PhotoImage(image)
-label1 = Label(left_frame, image=image).grid(row=1, column=0, padx=5, pady=5)
-first_image_generate(EXAMPLE_PATH)
+        else:
+            prediction = self.monet_generator(img, training=False)[0].numpy()
 
-root.mainloop()
+        prediction = (prediction * 127.5 + 127.5).astype(np.uint8)
+        generated_img = Image.fromarray(prediction)
+        generated_img = generated_img.resize(IMAGE_DISPLAY_SIZE)
+        generated_img = ImageTk.PhotoImage(generated_img)
+
+        self.label_generated = Label(self.right_frame, image=generated_img)
+        self.label_generated.image = generated_img
+        self.label_generated.grid(row=1, column=0, padx=5, pady=5)
+
+    def preprocess_image(self, img):
+        img = tf.io.decode_image(img)
+        img_rows, img_cols, channel = 256, 256, 3
+        return tf.reshape(tf.cast(tf.image.resize(img, (int(img_rows), int(img_cols))), tf.float32) / 127.5 - 1,
+                        (1, img_rows, img_cols, channel))
+
+    def selected_artist_changed(self):
+        self.generate_picture(self.original_img)
+
+    def load_image(self):
+        filename = easygui.fileopenbox(filetypes=["*.png", "*.jpg"]) # need to change so that they are both in one tab and
+                            # so that only files of these types can be selected (still better than GIFs, so its improvement)
+        image = Image.open(filename)
+        image = image.resize(IMAGE_DISPLAY_SIZE)
+        image = ImageTk.PhotoImage(image)
+
+        self.label_original = Label(self.left_frame, image=image)
+        self.label_original.image = image
+        self.label_original = self.label_original.grid(row=1, column=0, padx=5, pady=5)
+
+        img_to_generate_painting = tf.io.read_file(filename)
+        img_to_generate_painting = self.preprocess_image(img_to_generate_painting)
+        self.original_img = img_to_generate_painting
+        self.generate_picture(img_to_generate_painting)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
